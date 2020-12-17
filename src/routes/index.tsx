e375a08be88ad875c599fdce05f28f1875303e26
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-12-17 13:28:34 -0300

    Adding article page to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Article from '../pages/Article';
 import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/article" component={Article} />
   </Switch>
 );
 
