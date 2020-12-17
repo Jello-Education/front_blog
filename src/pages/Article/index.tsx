dc688da5653204b72297f4ad80ec3cdf4bd7ba3c
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-12-17 13:28:09 -0300

    Adding content to Article page and styling it

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,13 +1,60 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+  Container,
+  Main, 
+  Titles, 
+  Profile, 
+  User, 
+  Icons,
+  Icon
+} from './styles';
+
+import { FaWhatsapp, FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter  } from "react-icons/fa";
+
+import userFoto from '../../assets/userFoto.svg';
 
 const Article: React.FC = () => {
   return (
     <Container>
-      
+      <Main>
+        <Titles>
+          <p>Tipo de conteúdo (categoria)</p>
+
+          <h1>Titulo título título título</h1>
+          <sub>Subtitulo Subtitulo</sub>
+        </Titles>
+        <Profile>
+          <User>
+            <img src={userFoto} alt="userFoto"/>
+            <p>Por <b>Fulano de Tal</b></p>
+          </User>
+          <Icons>
+            <Icon>
+              <FaWhatsapp color="#fff" size={25}/>
+            </Icon>
+
+            <Icon>
+              <FaFacebookF color="#fff" size={25}/>
+            </Icon>
+
+            <Icon>
+              <FaInstagram color="#fff" size={25}/>
+            </Icon>
+
+            <Icon>
+              <FaLinkedinIn color="#fff" size={25}/>
+            </Icon>
+
+            <Icon>
+              <FaTwitter color="#fff" size={25}/>
+            </Icon>
+            
+          </Icons>
+        </Profile>
+      </Main>
     </Container>
   );
-}
+};
 
 export default Article;
--- src/pages/Article/styles.ts
+++ src/pages/Article/styles.ts
@@ -1,5 +1,78 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  margin-top: 80px;
+  height: 100vh;
+  width: 100%;
+  background: #e5e5e5;
+  text-align: left;
+  padding: 0 24px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Main = styled.div`
+  width: 1038px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Titles = styled.div`
+  margin-bottom: 52px;
+
+  p{
+    color: #fd3373;
+    text-decoration: underline;
+    margin-bottom: 48px;
+  }
+
+  h1{
+    font-size: 42px;
+    margin-bottom: 21px
+  }
+
+  sub{
+    font-size: 36px;
+  }
+`;
+
+export const Profile = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 52px;
+  border-bottom: 2px solid #fd3373;
+`;
+
+export const User = styled.div`
+  display: flex;
+  align-items: center;
+
+  img{
+    margin-right: 13px;
+  }
+
+  p{
+    font-size: 24px;
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Icon = styled.div`
+  height: 42px;
+  width: 42px;
+  border-radius: 50%;
+  background: #FD98B8;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
