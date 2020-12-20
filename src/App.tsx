a7ede26553934203b70722b4d260b0c7f5cf5e33
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-12-20 16:59:27 -0300

    Adding footer to the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './sytles/global';
 import Routes from './routes';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App: React.FC = () => (
   <>
     <BrowserRouter>
       <Header />
       <Routes />
+      <Footer />
     </BrowserRouter>
     <GlobalStyle />
   </>
