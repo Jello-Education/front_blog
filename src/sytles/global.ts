b21b47daaf9ec89b4b6e5e36ef7212b65f86462a
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-12-20 17:00:13 -0300

    Adding some styles to globalStyles

--- src/sytles/global.ts
+++ src/sytles/global.ts
@@ -13,7 +13,20 @@ export default createGlobalStyle`
     width: 100%;
 	}
 
+	html{
+		background: #E5E5E5;
+	}
+
 	button {
 		cursor: pointer;
 	}
+
+	li{
+		list-style: none;
+	}
+
+	h1, h2, h3, h4, h5, h6, p, b, a, button, span{
+		font-family: "Montserrat", sans-serif;
+	}
+
 `;
