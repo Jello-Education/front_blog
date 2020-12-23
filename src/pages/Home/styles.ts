bf0d3390d471867e5183028f0446dec66cd58c87
belapferreira <isabelapenhaferreira@gmail.com>
2020-12-23 10:36:05 -0300

    chore: correct background of post section

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -80,6 +80,7 @@ export const PostsContainer = styled.section`
   padding-left: 24px;
   width: 100%;
   margin: auto;
+  background: #fafafa;
 
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
