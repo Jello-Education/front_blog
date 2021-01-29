31f723cb9ea78aff23c8edcf07d0e1c3b81e3636
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-01-28 22:16:57 -0300

    Creating courses page

---
+++ src/pages/courses/index.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+
+import { Container, Section, Top, Texts, Input, Icon, Categories, Categorie } from '../../styles/courseStyles';
+import { FiSearch } from 'react-icons/fi';
+
+import Header from '../../components/Shared/Header';
+
+const courses: React.FC = () => {
+  return (
+    <Container>
+        <Header />
+        <Top>
+          <Section>
+            <Texts>
+              <h1>Estudar pode ser <b>divertido!</b></h1>
+
+              <Input>
+                <input placeholder="O que você deseja aprender hoje?" type="text"/>
+                <Icon>
+                  <FiSearch size={30} color="#fff"/>
+                </Icon>
+              </Input>
+            </Texts>
+            <Image 
+              src="/courses-example-img.svg"
+              width={440}
+              height={600}
+            />
+          </Section>
+
+          <Categories>
+            <Categorie>
+              <span>Categorias</span>
+            </Categorie>
+            <Categorie>
+              <span>Categorias</span>
+            </Categorie>
+            <Categorie>
+              <span>Categorias</span>
+            </Categorie>
+            <Categorie>
+              <span>Categorias</span>
+            </Categorie>
+            <Categorie>
+              <span>Categorias</span>
+            </Categorie>
+            <Categorie>
+              <span>Categorias</span>
+            </Categorie>
+          </Categories>
+        </Top> 
+    </Container>
+  );
+}
+
+export default courses;
