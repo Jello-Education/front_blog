878c22a3fe3b18787e4cc66b101b08d04a47e93f
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-03-12 14:28:36 -0300

    Changing about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import Image from 'next/image';
 
 import Header from '../../components/Shared/Header';
 import JelloInfo from '../../components/Shared/JelloInfo';
 import SEO from '../../components/Seo';
+import Courses from '../../components/About/Courses';
 
 import {
   Container,
   AboutJelloContainer,
   AboutJelloDetails,
-  Courses,
 } from '../../styles/aboutStyles';
 
 
@@ -27,47 +26,7 @@ const AboutJello: React.FC = () => {
         </AboutJelloDetails>
       </AboutJelloContainer>
 
-      <Courses>
-        <div className="first-description">
-          <div className="courses-left-first-description">
-            <h2>Como são os cursos?</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco{' '}
-            </p>
-          </div>
-
-          <div className="courses-right-first-description">
-            <Image 
-              src="/board-image.svg" 
-              alt="Quadro de cursos" 
-              width={506}
-              height={320}
-            />     
-          </div>
-        </div>
-
-        <div className="second-description">
-          <div className="courses-left-second-description">
-            <Image 
-              src="/study-image.svg" 
-              alt="Menina estudando" 
-              width={506}
-              height={320}
-              />
-          </div>
-
-          <div className="courses-right-second-description">
-            <h2>Como são os cursos?</h2>
-            <h3>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco{' '}
-            </h3>
-          </div>
-        </div>
-      </Courses>
+      <Courses />
 
       <JelloInfo />
     </Container>
