e68a653b20db441dcc4cafe271b5730610f66c8e
João Pedro de Carvalho <frontendcarvalho@gmail.com>
2021-11-16 20:03:10 -0300

    .

--- src/components/Courses/Categories/index.tsx
+++ src/components/Courses/Categories/index.tsx
@@ -2,26 +2,52 @@ import React from 'react';
 
 import { Container, Categorie } from './styles';
 
+const slide = [
+  {
+    id:1,
+    imagem: "./courses-card-img.svg",
+    Title: "teste",
+    specialty: "teste",
+  },
+  {
+    id:2,
+    imagem: "./courses-card-img.svg",
+    Title: "teste",
+    specialty: "teste",
+  },
+  {
+    id:3,
+    imagem: "./courses-card-img.svg",
+    Title: "teste",
+    specialty: "teste",
+  },
+  {
+    id:4,
+    imagem: "./courses-card-img.svg",
+    Title: "teste",
+    specialty: "teste",
+  },
+  {
+    id:5,
+    imagem: "./courses-card-img.svg",
+    Title: "teste",
+    specialty: "teste",
+  },
+  
+]
+
 const Categories: React.FC = () => {
   return (
     <Container>
       <Categorie>
-        <span>Categorias</span>
-      </Categorie>
-      <Categorie>
-        <span>Categorias</span>
-      </Categorie>
-      <Categorie>
-        <span>Categorias</span>
-      </Categorie>
-      <Categorie>
-        <span>Categorias</span>
-      </Categorie>
-      <Categorie>
-       <span>Categorias</span>
-      </Categorie>
-      <Categorie>
-        <span>Categorias</span>
+      <section id='section1'>
+                    {slide &&
+                      slide.map((item, key) => ( 
+                      <div className="item" key={key}>
+                        <img src={item.imagem} alt={item.Title}/>
+                      </div>
+                    ))}
+    </section>
       </Categorie>
     </Container>
   );
