0a3f35ca9bb91ef30f77e21e2b78258750135e10
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-01-30 16:29:22 -0300

    fix

--- src/components/Shared/Header/styles.ts
+++ src/components/Shared/Header/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Container = styled.header`
   position: fixed;
   left: 0rem;
@@ -14,14 +13,13 @@ export const Container = styled.header`
   border-radius: 0 0 20px 20px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
-
   .home {
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     font-weight: 500;
     font-size: 1.3rem;
     line-height: 24px;
 
-    color: #F24C4C;
+    color: #f24c4c;
     margin: 0 30px 4px;
   }
 
@@ -32,7 +30,6 @@ export const Container = styled.header`
     cursor: pointer;
     position: absolute;
     left: 633px;
-
   }
 
   @media (max-width: 1000px) {
@@ -52,7 +49,6 @@ export const Container = styled.header`
       left: 262px;
     }
   }
-
 `;
 
 export const HeaderContent = styled.div`
@@ -70,150 +66,142 @@ export const NavBar = styled.div`
   margin-top: 0.5rem;
   margin-left: 4rem;
 
-
-  *{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
-}
-.wrapper{
-  height: 100%;
-  width: 300px;
-  position: relative;
-}
-.wrapper .menu-btn{
-  position: absolute;
-  left: 20px;
-  top: -1.5em;
-  background: #FAFAFA;
-  color: #FD3373;
-  height: 45px;
-  width: 45px;
-  z-index: 9999;
-  border-radius: 5px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.3s ease;
-  
-}
-
-
-#btn:checked ~ .menu-btn{
-  left: 142px;
-  top: -1.5em;
-}
-
-@media (min-width: 1800px){
-
-  .wrapper .menu-btn{
-    left: -221px;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
+  .wrapper {
+    height: 100%;
+    width: 300px;
+    position: relative;
+  }
+  .wrapper .menu-btn {
+    position: absolute;
+    left: 20px;
+    top: -1.5em;
+    background: #fafafa;
+    color: #fd3373;
+    height: 45px;
+    width: 45px;
+    z-index: 9999;
+    border-radius: 5px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s ease;
   }
 
   #btn:checked ~ .menu-btn {
-    left: -136px;
-  }
-}
-
-@media (min-width: 810px) and (max-width: 900px) {
-  #btn:checked ~ .menu-btn {
-    left: 210px;
-  }
-}
-
-@media (min-width: 375px) and (max-width: 500px) {
-
-  .wrapper .menu-btn{
-    top: -2em;
+    left: 142px;
+    top: -1.5em;
   }
 
+  @media (min-width: 1800px) {
+    .wrapper .menu-btn {
+      left: -221px;
+    }
 
-  #btn:checked ~ .menu-btn {
-    left: 220px;
-    top: -2em;
+    #btn:checked ~ .menu-btn {
+      left: -136px;
+    }
   }
-}
-.wrapper .menu-btn i{
-  position: absolute;
-  transform: 0 ;
-  display: flex ;
-  font-size: 23px;
-  transition: all 0.3s ease;
-}
-.wrapper .menu-btn i.fa-times{
-  opacity: 0;
-}
-#btn:checked ~ .menu-btn i.fa-times{
-  color: #FD3373;
-  opacity: 1;
-  transform: rotate(-180deg);
-}
-#btn:checked ~ .menu-btn i.fa-bars{
-  color: #FD3373;
-  opacity: 0;
-  transform: rotate(180deg);
-}
-#sidebar{
-  position: fixed;
-  background: #FD3373;
-  height: 100%;
-  width: 270px;
-  overflow: hidden;
-  top: 0;
-  left: -270px;
-  transition: all 0.3s ease;
-}
-#btn:checked ~ #sidebar{
-  top: 0;
-  left: 0;
-}
-#sidebar .title{
-  line-height: 65px;
-  text-align: center;
-  background: #FD3373;
-  font-size: 25px;
-  font-weight: 600;
-  color: white;
-  border-bottom: 1px solid;
-}
-#sidebar .list-items{
-  cursor: pointer;
-  position: relative;
-  background: #FD3373;
-  width: 100%;
-  height: 100%;
-  list-style: none;
-}
-#sidebar .list-items li{
-  color: white;
-  padding-left: 40px;
-  line-height: 50px;
-  border-top: 1px solid whitesmoke;
-  border-bottom: 1px solid whitesmoke;
-  transition: all 0.3s ease;
-}
-#sidebar .list-items li:hover{
-  border-top: 1px solid transparent;
-  border-bottom: 1px solid transparent;
-  box-shadow: 0 0px 10px 3px whitesmoke;
-}
-#sidebar .list-items li:first-child{
-  border-top: none;
-}
-#sidebar .list-items li {
-  color: white;
-  text-decoration: none;
-  font-size: 18px;
-  font-weight: 500;
-  height: 100%;
-  width: 100%;
-  display: block;
-}
-#sidebar .list-items li{
-  margin-right: 20px;
-}
 
+  @media (min-width: 810px) and (max-width: 900px) {
+    #btn:checked ~ .menu-btn {
+      left: 210px;
+    }
+  }
 
+  @media (min-width: 375px) and (max-width: 500px) {
+    .wrapper .menu-btn {
+      top: -2em;
+    }
+
+    #btn:checked ~ .menu-btn {
+      left: 220px;
+      top: -2em;
+    }
+  }
+  .wrapper .menu-btn i {
+    position: absolute;
+    transform: 0;
+    display: flex;
+    font-size: 23px;
+    transition: all 0.3s ease;
+  }
+  .wrapper .menu-btn i.fa-times {
+    opacity: 0;
+  }
+  #btn:checked ~ .menu-btn i.fa-times {
+    color: #fd3373;
+    opacity: 1;
+    transform: rotate(-180deg);
+  }
+  #btn:checked ~ .menu-btn i.fa-bars {
+    color: #fd3373;
+    opacity: 0;
+    transform: rotate(180deg);
+  }
+  #sidebar {
+    position: fixed;
+    background: #fd3373;
+    height: 100%;
+    width: 270px;
+    overflow: hidden;
+    top: 0;
+    left: -270px;
+    transition: all 0.3s ease;
+  }
+  #btn:checked ~ #sidebar {
+    top: 0;
+    left: 0;
+  }
+  #sidebar .title {
+    line-height: 65px;
+    text-align: center;
+    background: #fd3373;
+    font-size: 25px;
+    font-weight: 600;
+    color: white;
+    border-bottom: 1px solid;
+  }
+  #sidebar .list-items {
+    cursor: pointer;
+    position: relative;
+    background: #fd3373;
+    width: 100%;
+    height: 100%;
+    list-style: none;
+  }
+  #sidebar .list-items li {
+    color: white;
+    padding-left: 40px;
+    line-height: 50px;
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+    transition: all 0.3s ease;
+  }
+  #sidebar .list-items li:hover {
+    border-top: 1px solid transparent;
+    border-bottom: 1px solid transparent;
+    box-shadow: 0 0px 10px 3px whitesmoke;
+  }
+  #sidebar .list-items li:first-child {
+    border-top: none;
+  }
+  #sidebar .list-items li {
+    color: white;
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: 500;
+    height: 100%;
+    width: 100%;
+    display: block;
+  }
+  #sidebar .list-items li {
+    margin-right: 20px;
+  }
 `;
